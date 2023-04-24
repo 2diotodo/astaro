2ac87bda51fc4acc0e8e4012246f4434d75f515f
qkr2713 <qkr2713@naver.com>
2023-04-24 13:24:38 +0900

    [BE-feat]Add card_info table

---
+++ backend/taro-service/src/main/java/com/a604/taroservice/data/CardInfo.java
@@ -0,0 +1,40 @@
+package com.a604.taroservice.data;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "card_info")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CardInfo {
+
+    @Id
+    @Column(nullable = false)
+    private Short seq;
+
+    @Size(max=20)
+    @Column(nullable = false)
+    private String name;
+
+    @Size(max=50)
+    private String keyword;
+
+    private String meaning;
+
+    @Size(max=100)
+    @Column(name="image_url")
+    private String imageUrl;
+
+    private Short good;
+
+    private Short bad;
+}
