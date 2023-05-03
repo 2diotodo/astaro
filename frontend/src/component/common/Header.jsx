029022a1ce4890fe33c6563c5ba893109aa61ce2
2diotodo <2diotodo@gmail.com>
2023-05-03 15:31:20 +0900

    [FE-fix] 헤더수정

--- frontend/src/component/common/Header.jsx
+++ frontend/src/component/common/Header.jsx
@@ -1,17 +1,39 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
-import { GiBlackHoleBolas, GiStarSwirl } from "react-icons/gi";
+import { GiStarSwirl } from "react-icons/gi";
+import {
+  MdOutlineChatBubbleOutline,
+  MdOutlineMarkChatUnread,
+} from "react-icons/md";
 import Navbar from "@component/common/Navbar";
+import { isLoginCheck } from "@features/commonSlice/loginSlice";
 import { toggleNavBar } from "@features/commonSlice/navSlice";
 
 function Header() {
   const navState = useSelector((state) => state.navBars);
   const dispatch = useDispatch();
 
+  // 로그인 여부 확인
+  const [isLogin, setIsLogin] = useState(() => {
+    const token = localStorage.getItem("token");
+    return !!token;
+  });
+
+  // 로그인상태 가져오기
+  const isLoginState = useSelector((state) => state.loginCheck);
+
   const navigate = useNavigate();
   // url 정보 받아오는 hook
   const location = useLocation();
+
+  // 읽지않은 채팅여부확인
+  const [unread, setUnread] = useState(false);
+
+  // unread 채팅알람기능 -구현 필요
+  const UnreadCheck = () => {
+    console.log("chat Alarm");
+  };
 
   // 해당 페이지로 이동 선언
   const navigateToMain = () => {
@@ -19,11 +41,15 @@ function Header() {
   };
 
   useEffect(() => {
+    dispatch(isLoginCheck(isLogin));
+  }, []);
+
+  useEffect(() => {
     dispatch(toggleNavBar(false));
   }, [dispatch, location.pathname, navigate]);
 
-  // 블랙홀 페이지로 이동-변경필요
-  const moveToBlackhole = () => {
+  // 채팅목록으로 이동-변경필요
+  const moveToMessageList = () => {
     navigate("/");
   };
   // toggle로 로그인상태관리
@@ -43,14 +69,27 @@ function Header() {
           <h1>Astaro</h1>
         </div>
         <div className={`navbar-wrapper ${navState.toggle ? "open" : "close"}`}>
-          <Navbar />
+          <Navbar setIsLogin={setIsLogin} isLoginState={isLoginState} />
         </div>
-        <GiBlackHoleBolas
-          onClick={moveToBlackhole}
-          color="white"
-          size="30px"
-          style={{ right: "60px" }}
-        />
+        {isLogin ? (
+          unread ? (
+            <MdOutlineMarkChatUnread
+              onClick={moveToMessageList}
+              color="white"
+              size="30px"
+              style={{ right: "60px" }}
+            />
+          ) : (
+            <MdOutlineChatBubbleOutline
+              onClick={moveToMessageList}
+              color="white"
+              size="30px"
+              style={{ right: "60px" }}
+            />
+          )
+        ) : (
+          ""
+        )}
         <GiStarSwirl
           onClick={toggleNavHandler}
           className={`cursor-pointer ${navState.toggle ? "open" : "close"}`}
