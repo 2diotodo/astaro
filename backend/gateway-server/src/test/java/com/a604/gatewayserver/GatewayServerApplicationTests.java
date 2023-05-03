0475174bb5411d142065fbda1c5f5e636468bc75
권영진 <dudwls624@naver.com>
2023-05-03 10:22:55 +0900

    [FE-feat] Gateway Server build & config

---
+++ backend/gateway-server/src/test/java/com/a604/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a604.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
