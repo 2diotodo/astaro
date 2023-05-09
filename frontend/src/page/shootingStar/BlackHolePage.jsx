1c2fcd8b9539988f941b50411b8fbc01c1ff85d8
1109ssh <1109ssh@naver.com>
2023-05-09 17:42:17 +0900

    [FE - feature] blackhole 소리인식

--- frontend/src/page/shootingStar/BlackHolePage.jsx
+++ frontend/src/page/shootingStar/BlackHolePage.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import BlackHoleInput from '@/component/shootingStar/BlackHoleInput';
-import { AiOutlineSend } from "react-icons/ai";
+import { AiOutlineSend, AiOutlineAudio } from "react-icons/ai";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import WaveSurfer from 'wavesurfer.js';
+import MicrophonePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.microphone.min.js';
 
 const rotate = keyframes`
   0% {
@@ -74,14 +77,94 @@ const SendButtonContainer = styled.div`
 	margin-right: 5rem;
 `;
 
+const AudioButton = styled.button`
+  z-index: 1;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: ${props => props.recording ? "red" : "white"};
+  font-size: 1.5rem;
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+`;
+
+const Visualizer = styled.div`
+  width: 100%;
+  height: 40px;
+  background: rgba(255, 255, 255, 0.5);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
 const BlackHolePage = () => {
+  const [recording, setRecording] = useState(false);
+  const { transcript, listening } = useSpeechRecognition();
+  const visualizerRef = useRef(null);
+  const wavesurferRef = useRef(null);
+
+  useEffect(() => {
+    if (recording) {
+      wavesurferRef.current = WaveSurfer.create({
+        container: visualizerRef.current,
+        waveColor: 'white',
+        progressColor: 'white',
+        cursorWidth: 0,
+        interact: false,
+        plugins: [MicrophonePlugin.create()]
+      });
+      wavesurferRef.current.microphone.start();
+    } else {
+      if (wavesurferRef.current) {
+        wavesurferRef.current.microphone.stop();
+        wavesurferRef.current.destroy();
+      }
+    }
+  
+    return () => {
+      if (wavesurferRef.current) {
+        wavesurferRef.current.microphone.stop();
+        wavesurferRef.current.destroy();
+      }
+    };
+  }, [recording]);
+
+  const startRecording = () => {
+    setRecording(true);
+    SpeechRecognition.startListening({ continuous: true });
+  };
+  
+  const stopRecording = () => {
+    setRecording(false);
+    SpeechRecognition.stopListening();
+  };
+
+  const toggleRecording = () => {
+    if (listening) {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+  };
+
   return (
     <Container>
       <Title>블랙홀</Title>
       <InputContainer>
-        <BlackHoleInput placeholder="여기에 고민을 적어주세요..." />
+        {recording ? (
+          <Visualizer ref={visualizerRef} />
+        ) : (
+          <BlackHoleInput value={transcript} placeholder="여기에 고민을 적어주세요..." />
+        )}
       </InputContainer>
       <SendButtonContainer>
+        <AudioButton recording={recording} onClick={toggleRecording}>
+          <AiOutlineAudio />
+        </AudioButton>
         <SendButton>
           <AiOutlineSend />
           <SendText>Send</SendText>
