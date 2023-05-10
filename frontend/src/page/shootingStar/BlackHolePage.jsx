09bfae7806a64d7afa4aed8730403529ff4e95ec
1109ssh <1109ssh@naver.com>
2023-05-10 16:17:46 +0900

    [FE - feature] 음성 반응하는 배경

--- frontend/src/page/shootingStar/BlackHolePage.jsx
+++ frontend/src/page/shootingStar/BlackHolePage.jsx
@@ -16,7 +16,8 @@ const rotate = keyframes`
 `;
 
 const Container = styled.div`
-  height: 100vh;
+  height: 90vh;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -33,7 +34,7 @@ const Container = styled.div`
     height: 100%;
     position: absolute;
     z-index: 0;
-    animation: ${rotate} 10s linear infinite;
+    animation: ${rotate} 30s linear infinite;
   }
 `;
 
@@ -90,8 +91,8 @@ const AudioButton = styled.button`
 `;
 
 const Visualizer = styled.div`
-  width: 100%;
-  height: 40px;
+  width: 800px;
+  height: 200px;
   background: rgba(255, 255, 255, 0.5);
   border-radius: 5px;
   display: flex;
@@ -105,33 +106,72 @@ const BlackHolePage = () => {
   const [recording, setRecording] = useState(false);
   const { transcript, listening } = useSpeechRecognition();
   const visualizerRef = useRef(null);
-  const wavesurferRef = useRef(null);
+  const canvasRef = useRef(null);
+  const audioContextRef = useRef(null);
+  const analyserRef = useRef(null);
+  const bufferLengthRef = useRef(null);
 
   useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const drawWave = () => {
+      if (!analyserRef.current || !canvasRef.current) return;
+      
+      const canvas = canvasRef.current;
+      const canvasCtx = canvas.getContext('2d');
+      const dataArray = new Uint8Array(bufferLengthRef.current);
+
+      requestAnimationFrame(drawWave);
+      analyserRef.current.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = 'rgba(255, 255, 255, 0)';
+      canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = 'black';
+      canvasCtx.beginPath();
+
+      const sliceWidth = (canvas.width * 1.0) / bufferLengthRef.current;
+      let x = 0;
+
+      for (let i = 0; i < bufferLengthRef.current; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = v * canvas.height / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height / 2);
+      canvasCtx.stroke();
+    };
+
     if (recording) {
-      wavesurferRef.current = WaveSurfer.create({
-        container: visualizerRef.current,
-        waveColor: 'white',
-        progressColor: 'white',
-        cursorWidth: 0,
-        interact: false,
-        plugins: [MicrophonePlugin.create()]
-      });
-      wavesurferRef.current.microphone.start();
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          audioContextRef.current = new AudioContext();
+          const source = audioContextRef.current.createMediaStreamSource(stream);
+          analyserRef.current = audioContextRef.current.createAnalyser();
+          analyserRef.current.fftSize = 2048;
+          bufferLengthRef.current = analyserRef.current.frequencyBinCount;
+
+          source.connect(analyserRef.current);
+          drawWave();
+        })
+        .catch(err => {
+          console.error('Error accessing audio stream:', err);
+        });
     } else {
-      if (wavesurferRef.current) {
-        wavesurferRef.current.microphone.stop();
-        wavesurferRef.current.destroy();
+      if (audioContextRef.current) {
+        audioContextRef.current.close();
       }
     }
-  
-    return () => {
-      if (wavesurferRef.current) {
-        wavesurferRef.current.microphone.stop();
-        wavesurferRef.current.destroy();
-      }
-    };
   }, [recording]);
+      
 
   const startRecording = () => {
     setRecording(true);
@@ -156,7 +196,9 @@ const BlackHolePage = () => {
       <Title>블랙홀</Title>
       <InputContainer>
         {recording ? (
-          <Visualizer ref={visualizerRef} />
+          <Visualizer ref={visualizerRef}>
+            <canvas ref={canvasRef} width="800" height="200" />
+          </Visualizer>
         ) : (
           <BlackHoleInput value={transcript} placeholder="여기에 고민을 적어주세요..." />
         )}
