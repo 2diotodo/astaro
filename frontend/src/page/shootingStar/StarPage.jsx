75e4cebbcf3ffccbf8ff2f998c58a79d198546b4
1109ssh <1109ssh@naver.com>
2023-05-15 16:37:56 +0900

    [FE, BE - feature] 별똥별 info 버튼, 채팅 닉네임 표시
    Former-commit-id: ffe5222f4eda25058d98fb1d96ff39962dcd8bf3

--- frontend/src/page/shootingStar/StarPage.jsx
+++ frontend/src/page/shootingStar/StarPage.jsx
@@ -8,6 +8,10 @@ import "@/css/swiper-custom.css";
 import ShootingStars from "@/component/shootingStar/ShootingStarBackground";
 import { useDispatch } from "react-redux";
 import { setCategory } from "@/features/shootingStarSlice/starSlice";
+import Typist from 'react-typist';
+import { keyframes } from 'styled-components';
+
+
 
 SwiperCore.use([Pagination]);
 
@@ -18,6 +22,16 @@ const Category = styled.div`
   transform: translate(-50%, -50%);
   text-align: center;
   font-size: 1.5rem;
+  color: white;
+`;
+
+const TypingMessage = styled(Typist)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-size: 1.3rem;
   color: white;
 `;
 
@@ -38,16 +52,51 @@ const StyledSwiper = styled(Swiper)`
   height: 100%;
 `;
 
+const blinking = keyframes`
+  from {
+    background-color: rgba(255, 255, 255, 0.5);
+    box-shadow: 0 0 30px rgba(255, 255, 255, 0.2);
+  }
+
+  to {
+    background-color: rgba(255, 255, 255, 0.8);    
+    box-shadow: 0 0 100px rgba(255, 255, 255, 0.8);
+  }
+`;
+
+const ShowMessageButton = styled.button`
+  position: absolute;
+  top: 8%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 5vw;
+  height: 3vh;
+  margin: 30px auto;
+  border-radius: 10px;
+  border: 0;
+  animation: ${blinking} 0.8s infinite alternate ease-in-out;
+  z-index: 5;
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
+
 const StarPage = () => {
   const [swiperIndex, setSwiperIndex] = useState(0);
   const [shootingStarsKey, setShootingStarsKey] = useState(0);
+  const [isTypingDone, setIsTypingDone] = useState(true);
+  const [showMessage, setShowMessage] = useState(false);
   const dispatch = useDispatch();
   const categories = ["결혼", "연애", "학업", "취직", "이직", "사업", "금전", "건강"];
 
+
   const handleSwiperChange = (index) => {
     setSwiperIndex(index);
-    console.log(categories[index])
     dispatch(setCategory(categories[index]));
+  };
+
+  const handleButtonClick = () => {
+    setShowMessage(true);
+    setIsTypingDone(false);
   };
 
   useEffect(() => {
@@ -59,6 +108,14 @@ const StarPage = () => {
       <Category>
         {categories[swiperIndex]}
       </Category>
+      <ShowMessageButton onClick={handleButtonClick}>
+        i
+      </ShowMessageButton>
+      {showMessage && !isTypingDone && (
+        <TypingMessage avgTypingDelay={200} onTypingDone={() => setTimeout(() => setIsTypingDone(true), 3000)}>
+          쏟아지는 별똥별을 잡아 다른 사람의 스토리를 확인하세요
+        </TypingMessage>
+      )}
       <StyledSwiper
         spaceBetween={50}
         slidesPerView={1}
