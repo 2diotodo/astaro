cc793504bdfe2078445ba4599dc1bb9f2da15d7b
권영진 <dudwls624@naver.com>
2023-05-15 13:16:25 +0900

    [FE-FIX] 회원가입 페이지 CSS 수정
    Former-commit-id: 38c3d96389d6d045d3e84a499b2cd47622128f4f

--- frontend/src/page/member/MemberSignup.jsx
+++ frontend/src/page/member/MemberSignup.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -9,13 +9,26 @@ import {
   duplicateNn,
   duplicateEm,
 } from "@features/memberSlice";
+import { MdCheckCircleOutline } from "react-icons/md";
 
 // Styled Component
+const FadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-50%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex:
   flex-direction: column;
   justify-content: start;
   align-items: center;
+
   position: absolute;
   height: 90%;
   width: 90%;
@@ -32,7 +45,7 @@ const Title = styled.div`
 `;
 const Message = styled.div`
   display: flex;
-  justify-content: start;
+  justify-content: center;
   align-items: center;
 
   padding: 5%;
@@ -41,45 +54,41 @@ const Message = styled.div`
   color: white;
 `;
 
-const InputWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-
-  height: 10%;
-
-  margin: 2%;
-`;
-
 const Label = styled.label`
-  width: 10%;
-  height: 90%;
-
-  font-size: 20px;
-  font-weight: 700;
-  color: white;
-  transition: all 0.2s;
+  font-size: 16px;
+  color: #fff;
 `;
 
 const Input = styled.input.attrs((props) => ({
   type: props.type || "text",
   placeholder: props.placeholder || "",
 }))`
-  width: 70%;
-  height: 90%;
+  width: 90%;
+  height: 30px;
 
-  border: none;
-  border-bottom: solid #aaaaaa 1px;
+  border: 0;
+  background: transparent;
 
-  color: white;
-  opacity: 0.5;
-  background: none;
+  font-size: 16px;
+  color: #fff;
 `;
 
-const SubmitBtn = styled.button`
-  width: 10%;
-  height: 90%;
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+
+  width: 80%;
+
+  margin-left: 10%;
+  margin-bottom: 5%;
+  padding-bottom: 5px;
+
+  border: 0;
+  border-bottom: 1px solid #555;
+
+  animation: ${FadeIn} 1s linear forwards;
 `;
 
 function MemberSignup() {
@@ -215,50 +224,75 @@ function MemberSignup() {
         {validateNn === true ? (
           <InputWrapper>
             <Input
-              placeholder={email}
+              placeholder="이메일"
               onChange={handleChangeEm}
               value={email}
             />
-            <button onClick={handleClickEm}>이메일 체크</button>
+            <MdCheckCircleOutline
+              onClick={handleClickEm}
+              color={validateEm ? "green" : "white"}
+              size={30}
+            />
           </InputWrapper>
         ) : null}
         {validatePw === true ? (
           <InputWrapper>
             <Input
-              placeholder={nickname}
+              placeholder="닉네임"
               onChange={handleChangeNn}
               value={nickname}
             />
-            <button onClick={handleClickNn}>닉네임 체크</button>
+            <MdCheckCircleOutline
+              onClick={handleClickNn}
+              color={validateNn ? "green" : "white"}
+              size={30}
+            />
           </InputWrapper>
         ) : null}
+
+        {/* 비밀번호 태그 */}
         {validateId === true ? (
-          <InputWrapper>
-            <Input
-              placeholder={password}
-              onChange={handleChangePw}
-              value={password}
-            />
-            <Input
-              placeholder={passwordConfirm}
-              onChange={handleChangePwConfirm}
-              value={passwordConfirm}
-            />
-          </InputWrapper>
+          <>
+            <InputWrapper>
+              <Input
+                placeholder="비밀번호"
+                onChange={handleChangePw}
+                value={password}
+                type="password"
+              />
+            </InputWrapper>
+            <InputWrapper>
+              <Input
+                placeholder="비밀번호 체크"
+                onChange={handleChangePwConfirm}
+                value={passwordConfirm}
+                type="password"
+              />
+              <MdCheckCircleOutline
+                onClick={handleClickId}
+                color={validatePw ? "green" : "white"}
+                size={30}
+              />
+            </InputWrapper>
+          </>
         ) : null}
+
+        {/* ID 태그 */}
         <InputWrapper>
-          <Label>Id</Label>
           <Input
-            placeholder={memberId}
+            placeholder="아이디"
             onChange={handleChangeId}
             value={memberId}
           />
-          <SubmitBtn onClick={handleClickId}>체크</SubmitBtn>
+          <MdCheckCircleOutline
+            onClick={handleClickId}
+            color={validateId ? "green" : "white"}
+            size={30}
+          />
         </InputWrapper>
+
         {validateEm === true ? (
-          <InputWrapper>
-            <button onClick={handleClickSubmit}>회원가입</button>
-          </InputWrapper>
+          <button onClick={handleClickSubmit}>회원가입</button>
         ) : null}
       </Wrapper>
     </>
