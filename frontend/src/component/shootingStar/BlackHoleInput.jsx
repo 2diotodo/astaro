a0868e978b979da6ffc1726eb138864cf25b84ba
김정민 <kjm3495@naver.com>
2023-05-18 16:19:22 +0900

    [FE-feat] 블랙홀 : stt 적용 및 사라지는 css 적용
    Former-commit-id: a0fd93b7fdb869652f9195923f5e4b1eddc6875d

--- frontend/src/component/shootingStar/BlackHoleInput.jsx
+++ frontend/src/component/shootingStar/BlackHoleInput.jsx
@@ -1,29 +1,67 @@
-// src/component/shootingStar/BlackHoleInput.jsx
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useRef, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
+const smoky = keyframes`
+  60% {
+    text-shadow: 0 0 40px whitesmoke;
+  }
+  to {
+    transform:
+      translate3d(15rem,-8rem,0)
+      rotate(-40deg)
+      skewX(70deg)
+      scale(0.5);
+    text-shadow: 0 0 20px whitesmoke;
+    opacity: 0;
+  }
+`;
+
+const smokyMirror = keyframes`
+  60% {
+    text-shadow: 0 0 40px whitesmoke;
+  }
+  to {
+    transform:
+      translate3d(18rem,-8rem,0)
+      rotate(-40deg)
+      skewX(-70deg)
+      scale(2);
+    text-shadow: 0 0 20px whitesmoke;
+    opacity: 0;
+  }
+`;
+
+const SmokyText = styled.div`
+  display: inline-block;
+  text-shadow: 0 0 0 whitesmoke;
+  animation: ${({ flag }) => flag ? css`${smoky} 7s 0s both` : 'none'};
+  &.hideText {
+    visibility: hidden;
+  }
+  & span:nth-child(even) {
+    animation-name: ${smokyMirror};
+  }
+  height: 100%;
+  width: 100%;
+  border-left: 0px;
+  border-right: 0px;
+  padding: 1rem;
+  color: gray;
+  background-color: rgba(0, 0, 0, 0);
+  outline: none;
+  resize: none;
+  font-size: 1rem;
+  margin-top: 20rem;
+  font-family: "TAEBAEKmilkyway";
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+`;
 
 const InputContainer = styled.div`
   width: 80%;
   display: flex;
   align-items: center;
   position: relative;
-`;
-
-const Input = styled.textarea`
-  width: 100%;
-  height: 12rem;
-  border-top: 0.5px solid white;
-  border-bottom: 0.5px solid white;
-  border-left: 0px;
-  border-right: 0px;
-  padding: 1rem;
-  color: white;
-  background-color: rgba(0, 0, 0, 0);
-  outline: none;
-  resize: none;
-  font-size: 1rem;
-  margin-top: 1rem;
-  font-family: "TAEBAEKmilkyway";
 `;
 
 const SendButton = styled.button`
@@ -36,10 +74,16 @@ const SendButton = styled.button`
   color: white;
 `;
 
-const BlackHoleInput = ({ placeholder }) => {
+const BlackHoleInput = ({ value, flag, placeholder }) => {
+  const [showText, setShowText] = useState(true);
+
   return (
     <InputContainer>
-      <Input placeholder={placeholder} />
+      <SmokyText flag={flag} className={showText ? "" : "hideText"} >
+        {value.split("").map((char, index) => (
+          <span key={index}>{char}</span>
+        ))}
+      </SmokyText>
       <SendButton>
         <i className="fas fa-paper-plane"></i>
       </SendButton>
